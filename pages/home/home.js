dbc587aab421c597f8e266b9d12467b981752924
haisen <49050590+karenzhangxd@users.noreply.github.com>
2020-10-18 17:15:39 +0800

    小程序网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+// 导入封装的网络请求接口
+import request from '../../service/network'
 Page({
 
   /**
@@ -12,6 +14,32 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+// 使用原生发送网络请求
+wx.request({
+  url: 'http://127.0.0.1:3000/pipeline/getNums',
+  success:(res)=>{
+    console.log(res);
+  }
+}),
+// post請求 攜帶參數
+wx.request({
+  url: 'http://127.0.0.1:3000/candidate/getCandidateByPipeline',
+  method:"POST",
+  data:{
+    pipeline:"推荐"
+  },
+  success:(res)=>{
+    console.log(res);
+  }
+})
+// 使用封装的request发送网请求
+request({url: 'http://127.0.0.1:3000/pipeline/getNums'}).then(res=>{
+  console.log(res);
+  
+}).catch(err=>{
+  console.log(err);
+  
+})
 
   },
 
@@ -40,7 +68,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
