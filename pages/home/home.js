7a07f14aa82b7c86713f9f25ceff4f860d36c566
haisen <49050590+karenzhangxd@users.noreply.github.com>
2020-10-18 17:45:02 +0800

    小程序弹窗展示

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,51 @@
 // pages/home/home.js
 Page({
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中',
+      // mask:true,
+      duration:3000,
+      icon:"loading"
+      
+    })
+
+  },
+  handleShowModel(){
+    wx.showModal({
+      title:"你好",
+      content:"内容",
+      // showCancel:false,
+      cancelText:"返回",
+      success:(res)=>{
+        console.log(res);
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+        
+      }
+    })
+  },
+  handleShowLoading(){
+    wx.showLoading({
+      title: 'loading',
+    }),
+    // 必须手动调用hideLoading才会消失
+    setTimeout(()=>{wx.hideLoading({
+      success: (res) => {},
+    })},1000)
+  },
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ["相册","拍照"],
+      success:(res)=>{
+        console.log(res);
+        // tapIndex用户点击了哪个选项
+        
+      }
+    })
+  },
 
   /**
    * 页面的初始数据
